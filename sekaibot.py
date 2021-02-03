1cd68bd1b423359d8872831728af346efbce61b9
HaswellAoba <42887520+HaswellAoba@users.noreply.github.com>
2021-02-04 01:48:15 +0900

    Major Update on Border Frame
    Changed border frame, now FHD resolution, bold font for score

--- sekaibot.py
+++ sekaibot.py
@@ -9,15 +9,16 @@ from PIL import Image,ImageDraw,ImageFont
 from datetime import datetime
 
 #Variable Setting
-current_event_id='11' #지금 진행중인 이벤트 ID
-current_event_name = 'Colour of Myself !' #지금 진행중인 이벤트 이름
-event_start_date = '2021-01-21 16:00' #이벤트 시작하는 날짜
-event_end_date = '2021-01-29 21:00' #이벤트 끝나는 날짜
+current_event_id='12' #지금 진행중인 이벤트 ID
+current_event_name = 'Period of NOCTURNE' #지금 진행중인 이벤트 이름
+event_start_date = '2021-01-31 16:00' #이벤트 시작하는 날짜
+event_end_date = '2021-02-07 21:00' #이벤트 끝나는 날짜
 
 today = datetime.today().strftime('%Y-%m-%d %H:%M')
 dataurl = 'https://bitbucket.org/sekai-world/sekai-event-track/raw/main/event{0}.json'.format(current_event_id)
 template_image = Image.open(os.path.join(sys.path[0], 'frame_{0}.png').format(current_event_id))
-font = ImageFont.truetype(os.path.join(sys.path[0], 'NotoSansCJKkr-Regular.otf'),24)
+font = ImageFont.truetype(os.path.join(sys.path[0], 'NotoSansCJKkr-Regular.otf'),30)
+scrfont = ImageFont.truetype(os.path.join(sys.path[0], 'NotoSansCJKkr-Medium.otf'),30)
 message = '이벤트 "{0}" 순위 경계 정보입니다. (기준시간 {1})'.format(current_event_name, today)
 tokens = {'consumer_key': '', 'consumer_secret': '', 'access_token': '', 'access_token_secret': ''}
 
@@ -48,9 +49,9 @@ api = tweepy.API(auth)
 
 #Image Setup
 draw = ImageDraw.Draw(template_image)
-name_dx = 92
-score_dx = 382
-dy = 300
+name_dx = 181
+score_dx = 546
+dy = 379
 
 #Get Current Ranking Data
 print("프로젝트 세카이 현재 이벤트 랭킹 파서")
@@ -91,10 +92,10 @@ for list in Top3Array:
 
     print('{0}위: {1}'.format(seq_no, finalstring))
     print('Template에 순위를 작성합니다')
-    draw.text((name_dx,(dy-seq_no)),list.get('name'),fill="black",font=font,align='center')
-    draw.text((score_dx,(dy-seq_no)),finalstring,fill="black",font=font,align='center')
+    draw.text((name_dx, dy),list.get('name'),fill="black",font=font,align='center')
+    draw.text((score_dx, dy),finalstring,fill="black",font=scrfont,align='center')
     seq_no+=1
-    dy+=38
+    dy+=53
     if(seq_no==4):
         break
 print()
@@ -102,7 +103,7 @@ print()
 #Rank 10~50
 print ("10~50위 포인트 리스트")
 rank_specify=20
-dy=439
+dy=555
 #Specificy for Rank 10
 for i in Top3Array:
     if i['rank'] == 10:
@@ -115,8 +116,8 @@ for i in Top3Array:
         print('10위: {0}'.format(finalstring))
         print('Template에 순위를 작성합니다')
         draw.text((name_dx,(dy-1)),i['name'],fill="black",font=font,align='center')
-        draw.text((score_dx,(dy-1)),finalstring,fill="black",font=font,align='center')
-        dy+=34
+        draw.text((score_dx,(dy-1)),finalstring,fill="black",font=scrfont,align='center')
+        dy+=49
 
 for i in range(1, 5):
     TempArray = json_data.get('rank{0}'.format(rank_specify))
@@ -130,16 +131,16 @@ for i in range(1, 5):
 
     print('{0}위: {1} - {2}'.format(rank_specify, list.get('name'), finalstring))
     print('Template에 순위를 작성합니다')
-    draw.text((name_dx,(dy-i)),list.get('name'),fill="black",font=font,align='center')
-    draw.text((score_dx,(dy-i)),finalstring,fill="black",font=font,align='center')
-    dy+=34
+    draw.text((name_dx,dy),list.get('name'),fill="black",font=font,align='center')
+    draw.text((score_dx,dy),finalstring,fill="black",font=scrfont,align='center')
+    dy+=49
     rank_specify+=10
 print()
 
 #Rank 100~500
 print("100~500위 포인트 리스트")
 rank_specify=100
-dy=634
+dy=821
 for i in range(1, 6):
     TempArray = json_data.get('rank{0}'.format(rank_specify))
     TempOld = json_old.get('rank{0}'.format(rank_specify))
@@ -152,16 +153,19 @@ for i in range(1, 6):
 
     print('{0}위: {1} - {2}'.format(rank_specify, list.get('name'), finalstring))
     print('Template에 순위를 작성합니다')
-    draw.text((name_dx,(dy-i)),list.get('name'),fill="black",font=font,align='center')
-    draw.text((score_dx,(dy-i)),finalstring,fill="black",font=font,align='center')
-    dy+=34
+    draw.text((name_dx,dy),list.get('name'),fill="black",font=font,align='center')
+    draw.text((score_dx,dy),finalstring,fill="black",font=scrfont,align='center')
+    dy+=49
     rank_specify+=100
 print()
+
 
 #Rank 1000~5000
 print("1000~5000위 포인트 리스트")
 rank_specify=1000
-dy=826
+name_dx=1057
+score_dx=1424
+dy=369
 for i in range(1, 6):
     TempArray = json_data.get('rank{0}'.format(rank_specify))
     TempOld = json_old.get('rank{0}'.format(rank_specify))
@@ -174,15 +178,15 @@ for i in range(1, 6):
 
     print('{0}위: {1} - {2}'.format(rank_specify, list.get('name'), finalstring))
     print('Template에 순위를 작성합니다')
-    draw.text((name_dx,(dy-i)),list.get('name'),fill="black",font=font,align='center')
-    draw.text((score_dx,(dy-i)),finalstring,fill="black",font=font,align='center')
-    dy+=34
+    draw.text((name_dx,dy),list.get('name'),fill="black",font=font,align='center')
+    draw.text((score_dx,dy),finalstring,fill="black",font=scrfont,align='center')
+    dy+=49
     rank_specify+=1000
 print()
 
 #Rank 10000~50000
 print("10000~50000위 포인트 리스트")
-dy=1012
+dy=632
 rank_specify=10000
 for i in range(1, 6):
     TempArray = json_data.get('rank{0}'.format(rank_specify))
@@ -196,27 +200,32 @@ for i in range(1, 6):
 
     print('{0}위: {1} - {2}'.format(rank_specify, list.get('name'), finalstring))
     print('Template에 순위를 작성합니다')
-    draw.text((name_dx,(dy-i)),list.get('name'),fill="black",font=font,align='center')
-    draw.text((score_dx,(dy-i)),finalstring,fill="black",font=font,align='center')
-    dy+=34
+    draw.text((name_dx,dy),list.get('name'),fill="black",font=font,align='center')
+    draw.text((score_dx,dy),finalstring,fill="black",font=scrfont,align='center')
+    dy+=49
     rank_specify+=10000
 print()
 
-#파일에 지금 날짜 기록
-draw.text((37,1200), ("기준: "+today), fill="black", font=font, align='center')
+#DRAW TODAY
+texttoday = datetime.today().strftime('%Y년 %m월 %d일 %H시 %M분 기준')
+draw.text((993,1007), texttoday, fill="black", font=font, align='center')
 
 #완성된 파일 저장
 template_image.save(os.path.join(sys.path[0], 'border.png'))
 
+
 #트윗
 print('트윗으로 작성합니다, 조금만 기다려 주십시오...')
+
 media_ids = []
 borderimg = api.media_upload(os.path.join(sys.path[0], 'border.png'))
 media_ids.append(borderimg.media_id)
 print(('미디어 업로드 OK : {0}').format(media_ids))
 api.update_status(status=message, media_ids=media_ids)
 
+
 print('Tweet OK')
+
 
 #기존 파일은 old로 저장
 try:
