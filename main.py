c7d9f8c30d2b8c048ab7c94079c5df8205a6afda
HaswellAoba <42887520+HaswellAoba@users.noreply.github.com>
2021-06-09 12:03:11 +0900

    Do not upload when obtained data is too old

--- main.py
+++ main.py
@@ -9,19 +9,24 @@ import initial_functions
 import main_functions
 from datetime import datetime
 
+def old():
+    oldmessage = '[Error]'
+    tweet(oldmessage, 'old')
+    exit()
+
 def download_data(dataurl):
     time.sleep(5)
     response = requests.get(dataurl)
     print('data.json 파일에 기록합니다...')
     open(os.path.join(sys.path[0], 'resources/data.json'), 'wb').write(response.content)
 
-def tweet(message):
+def tweet(message, image):
     print('트윗으로 작성합니다, 조금만 기다려 주십시오...')
     auth = tweepy.OAuthHandler(config.twtr_consumer_key, config.twtr_consumer_secret)
     auth.set_access_token(config.twtr_access_token, config.twtr_access_secret)
     api = tweepy.API(auth)
     media_ids = []
-    borderimg = api.media_upload(os.path.join(sys.path[0], 'border.png'))
+    borderimg = api.media_upload(os.path.join(sys.path[0], image+'.png'))
     media_ids.append(borderimg.media_id)
     print(('미디어 업로드 OK : {0}').format(media_ids))
     api.update_status(status=message, media_ids=media_ids)
@@ -33,6 +38,7 @@ def main(status):
     message = '이벤트 "{0}" 순위 경계 정보입니다. (기준시간 {1})'.format(config.current_event_name, today)
     if (status==1):
         message = '이벤트 "{0}" 에 대한 마지막 경계 정보입니다.'.format(config.current_event_name)
+    ms = int(round(time.time()*1000))
 
     print('\n랭킹 데이터 다운로드')
     download_data(dataurl)
@@ -40,7 +46,14 @@ def main(status):
     #Load Local JSON File
     with open(os.path.join(sys.path[0], 'resources/data.json'), 'r', encoding="UTF-8") as f:
         json_data = json.load(f)
+    
+    #Compare Time
+    current = json_data.get('time')
+    if (ms-current > 120000):
+        print('너무 오래된 데이터입니다.')
+        old()
 
+    #Trying with older data json
     try:
         with open(os.path.join(sys.path[0], 'resources/data_old.json'), 'r', encoding="UTF-8") as f:
             json_old = json.load(f)
@@ -56,7 +69,7 @@ def main(status):
         main_functions.main(json_data, json_old)
 
     #Tweet
-    tweet(message)
+    tweet(message, 'border')
 
     #Move current file to legacy
     try:
